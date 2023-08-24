72b4a26833445803ba8d5ed4ece03e394ce4c8e7
Nguyen Le Hoang Dung <dungchan321@gmail.com>
2023-08-24 17:12:07 +0700

    TimGiaTriTrongMang

---
+++ TimGiaTriTrongMang/Program.cs
@@ -0,0 +1,15 @@
+﻿
+class Program{
+    static void Main(string[] args)
+    {
+        String[] list={"Christian", "Michael", "Camila", "Sienna", "Tanya", "Connor", "Zachariah", "Mallory", "Zoe", "Emily"};
+        
+        Console.Write("Enter student name: ");
+        String name=Console.ReadLine();
+
+        if(list.Contains(name))
+            Console.WriteLine("Position of "+name+" in the list is "+(Array.IndexOf(list,name)+1));
+        else    
+            Console.WriteLine("Not found " + name + " in the list.");
+    }
+}
