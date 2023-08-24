8fbf671e5071ff6f05be04c9451ec22189915096
Nguyen Le Hoang Dung <dungchan321@gmail.com>
2023-08-24 16:38:14 +0700

    KiemTraSoNguyenTo

---
+++ KiemTraSoNguyenTo/Program.cs
@@ -0,0 +1,22 @@
+﻿
+class Program{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter a number: ");
+        int number=Convert.ToInt32(Console.ReadLine());
+
+        Boolean check=true;
+
+        if(number<2)
+            check=false;
+
+        for(int i=2;i<=number/2;i++)
+            if(number%i==0)
+                check=false;
+
+        if(check)
+            Console.WriteLine($"{number} is a prime number");
+        else 
+            Console.WriteLine($"{number} is not a prime number");
+    }
+}
