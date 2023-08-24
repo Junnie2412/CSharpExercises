990b1909f98e51a5d569c04c8eea9754b7e4b319
Nguyen Le Hoang Dung <dungchan321@gmail.com>
2023-08-24 17:12:25 +0700

    TimGiaTriLonNhatTrongMang

---
+++ TimGiaTriLonNhatTrongMang/Program.cs
@@ -0,0 +1,28 @@
+﻿
+class Program{
+    static void Main(string[] args)
+    {
+        int n;
+
+        do{
+            Console.Write("Enter n <=20: ");
+            n=Int32.Parse(Console.ReadLine());
+        }while(n>20 || n<=0);
+
+        int[] arr=new int[n];
+        
+        for(int i=0;i<n;i++){
+            Console.Write("a["+i+"]= ");
+            arr[i]=Int32.Parse(Console.ReadLine());
+        }
+
+        int max=0;
+
+        for(int i=0;i<n;i++)
+            if(arr[max]<arr[i])
+                max=i;
+
+        Console.WriteLine("Max value: "+arr[max]+" and its position "+(max+1));
+
+    }
+}
