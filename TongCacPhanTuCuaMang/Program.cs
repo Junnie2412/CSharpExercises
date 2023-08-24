e1179bf9bded15edc671724cd61252f39d06e4e8
Nguyen Le Hoang Dung <dungchan321@gmail.com>
2023-08-24 17:11:55 +0700

    TongCacPhanTuCuaMang

---
+++ TongCacPhanTuCuaMang/Program.cs
@@ -0,0 +1,19 @@
+﻿
+class Program{
+    static void Main(string[] args)
+    {
+        Console.Write("Nhap n : ");
+        int n=Int32.Parse(Console.ReadLine());
+
+        int[] arr=new int[n];
+        int sum=0;
+
+        for(int i=0;i<n;i++){
+            Console.Write("a["+i+"]= ");
+            arr[i]=Int32.Parse(Console.ReadLine());
+            sum+=arr[i];
+        }
+
+        Console.WriteLine("Sum is: "+sum);
+    }
+}
