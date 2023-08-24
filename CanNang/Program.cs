13988eca2193ea0085a718c9e6ad3b5db634f19b
Nguyen Le Hoang Dung <dungchan321@gmail.com>
2023-08-24 16:37:07 +0700

    CanNang

---
+++ CanNang/Program.cs
@@ -0,0 +1,27 @@
+﻿
+using System.Reflection.Metadata.Ecma335;
+
+namespace CanNang{
+    class Program{
+        static void Main(string[] args)
+        {
+            Console.Write("Enter height: ");
+            double height=Double.Parse(Console.ReadLine());
+
+            Console.Write("Enter weight: ");
+            double weight=Double.Parse(Console.ReadLine());
+
+            double bmi=weight / Math.Pow(height,2);
+            Console.Write("Your BIM is: "+bmi);
+
+            if(bmi<18)
+                Console.WriteLine("Underweight");
+            else if(bmi<25.0)
+                Console.WriteLine("Normal");
+            else if(bmi<30.0)
+                Console.WriteLine("Overweight"); 
+            else 
+                Console.WriteLine("Obese");   
+        }
+    }
+}
