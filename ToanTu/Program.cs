031dbacb308c3cf4f1d8845f2bfd4642e9ddc704
Nguyen Le Hoang Dung <dungchan321@gmail.com>
2023-08-24 16:37:26 +0700

    ToanTu

---
+++ ToanTu/Program.cs
@@ -0,0 +1,15 @@
+﻿
+namespace ToanTu{
+    class Program{
+        static void Main(string[] args)
+        {
+            Console.Write("Enter height: ");
+            float height=Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter width: ");
+            float width=Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Area is: "+height*width);
+        }
+    }
+}
