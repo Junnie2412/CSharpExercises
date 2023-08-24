fc72de4e6796992b23b4076ecc496c0dc9f1da71
Nguyen Le Hoang Dung <dungchan321@gmail.com>
2023-08-24 16:37:36 +0700

    HienThiCacLoaiHinh

---
+++ HienThiCacLoaiHinh/Program.cs
@@ -0,0 +1,38 @@
+﻿
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter n: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("The Rectangle: ");
+        for (int i = 1; i <= n; i++)
+        {
+            for (int k = 1; k <= n; k++)
+                Console.Write("* ");
+            Console.WriteLine();
+        }
+
+        Console.WriteLine();
+
+        Console.WriteLine("The Bottom Left Triangle right angle: ");
+        for (int i = 1; i <= n; i++)
+        {
+            for (int k = 1; k <= i; k++)
+                Console.Write("* ");
+            Console.WriteLine();
+        }
+
+        Console.WriteLine();
+
+        Console.WriteLine("The Top Left Triangle right angle: ");
+        for (int i = n; i >= 1; i--)
+        {
+            for (int k = 1; k <= i; k++)
+                Console.Write("* ");
+            Console.WriteLine();
+        }
+
+    }
+}
