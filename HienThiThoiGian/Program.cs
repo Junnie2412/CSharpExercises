89b1d809915c25964bb3d6211a7f1171b9d68fb2
Nguyen Le Hoang Dung <dungchan321@gmail.com>
2023-08-24 16:38:23 +0700

    HienThiThoiGian

---
+++ HienThiThoiGian/Program.cs
@@ -0,0 +1,10 @@
+﻿
+namespace HienThiThoiGian{
+    class Program{
+        static void Main(string[] args)
+        {
+            DateTime date=DateTime.Now;
+            Console.WriteLine("Date Time: "+date);
+        }
+    }
+}
