862b46822e05a9990bcb1ea41252640c5a1c420e
Nguyen Le Hoang Dung <dungchan321@gmail.com>
2023-08-24 16:38:35 +0700

    HienThiLoiChao

---
+++ HienThiLoiChao/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HienThiLoiChao{
+    class Program{
+        static void Main(string[] args)
+        {
+            Console.Write("Enter  your name: ");
+            String name=Console.ReadLine();
+            Console.WriteLine("Hello "+name);
+        }
+    }
+}
