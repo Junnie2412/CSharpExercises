43d590280515065cc8cc0e7d20fe10366bc6f4d7
Nguyen Le Hoang Dung <dungchan321@gmail.com>
2023-08-24 16:37:43 +0700

    TienTe

---
+++ TiLeTienTe/Program.cs
@@ -0,0 +1,11 @@
+﻿
+namespace TiLeTienTe{
+    class Program{
+        static void Main(string[] args)
+        {
+            Console.Write("Enter USD: ");
+            long usd=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(usd+" USD to VND is: "+usd*23000);
+        }
+    }
+}
