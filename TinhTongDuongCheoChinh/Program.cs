0620b17f90ee3579133f8915319aed64f6b40b4d
Nguyen Le Hoang Dung <dungchan321@gmail.com>
2023-08-25 16:36:39 +0700

    TinhTongDuongCheoChinh

---
+++ TinhTongDuongCheoChinh/Program.cs
@@ -0,0 +1,37 @@
+﻿
+
+class Program{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter axa matrix: ");
+        
+        Console.Write("Enter a: ");
+        int a=Int32.Parse(Console.ReadLine());
+
+        int[,] matrix=new int[a,a];
+
+        for(int i=0;i<a;i++){
+            for(int k=0;k<a;k++)
+            {
+                Console.Write("matrix["+i+";"+k+"]= ");
+                matrix[i,k]=Int32.Parse(Console.ReadLine());
+            }
+        }
+
+
+        Console.WriteLine("We have matrix: ");
+
+        int sum=0;
+
+        for(int i=0;i<a;i++){
+            for(int k=0;k<a;k++){
+                Console.Write(matrix[i,k]+" ");
+                if(i==k)
+                    sum+=matrix[i,k];
+            }
+            Console.WriteLine();
+        }
+
+        Console.Write("Sum of arr: "+sum);
+    }
+}
