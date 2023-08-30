eea1a263ab67c894e1cc6513b34aa65b6f044044
Nguyen Le Hoang Dung <dungchan321@gmail.com>
2023-08-30 15:48:10 +0700

    TimGTNNTrongMang

---
+++ TimGTNNTrongMang/Program.cs
@@ -0,0 +1,28 @@
+﻿
+class Program{
+
+    public static int findMin(int[] arr){
+        int min=arr[0];
+
+        for(int i=1;i<arr.Length;i++)
+            if(arr[i]<min)
+                min=arr[i];
+
+        return min;
+    }
+    static void Main(string[] args)
+    {
+        Console.Write("Enter n of array: ");
+        int n=Int32.Parse(Console.ReadLine());
+
+        int[] arr=new int[n];
+
+        for(int i=0;i<n;i++)
+        {
+            Console.Write("a["+i+"]= ");
+            arr[i]=Int32.Parse(Console.ReadLine());
+        }
+
+        Console.WriteLine("Min of array is: "+findMin(arr));
+    }
+}
