73c9afc8ae6a26779fbd98cfad323e24a7b4c0a0
Nguyen Le Hoang Dung <dungchan321@gmail.com>
2023-08-30 15:47:34 +0700

    DemSoLanXuatHienKyTu

---
+++ DemSoLanXuatHienKyTu/Program.cs
@@ -0,0 +1,25 @@
+﻿
+class Program{
+
+    public static int NumberOfOccurrences(String str, char c){
+
+        int count=0;
+
+        for(int i=0;i<str.Length;i++)
+            if(str[i].Equals(c))
+                count++;
+        
+        return count;
+    }
+    static void Main(string[] args)
+    {
+        Console.Write("Enter String: ");
+        String str=Console.ReadLine();
+
+        Console.Write("Enter letter to count the number of occurrences: ");
+        char c=char.Parse(Console.ReadLine());
+
+        Console.WriteLine("The number of occurrrences of "+c+" is "+NumberOfOccurrences(str, c));
+
+    }
+}
