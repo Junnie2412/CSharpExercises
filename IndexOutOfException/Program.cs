9a8e17bb5d43e81be08d30ae26f9b31394a20191
Nguyen Le Hoang Dung <dungchan321@gmail.com>
2023-09-14 18:59:18 +0700

    IndexOutOfException

---
+++ IndexOutOfException/Program.cs
@@ -0,0 +1,17 @@
+﻿
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            List<char> characters = new List<char>();
+            characters.InsertRange(0, new Char[] { 'a', 'b', 'c', 'd', 'e', 'f' });
+            int value = characters[6];
+        }
+        catch(IndexOutOfRangeException ex)
+        {
+            Console.WriteLine(ex);
+        }
+    }
+}
